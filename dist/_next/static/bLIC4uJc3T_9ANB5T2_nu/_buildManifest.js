672bc8c4832ebc6c1cbc012ac94fb4cec060e73a
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2022-08-23 14:12:16 +0900

    deploy

--- dist/_next/static/bLIC4uJc3T_9ANB5T2_nu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-ce69e74e8595c805.js","static/css/94d93e67a5783e67.css","static/chunks/pages/index-167e29d63b99adb4.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ dist/_next/static/lB3CoiA02FzYGe9TiSnbT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-ce69e74e8595c805.js","static/css/94d93e67a5783e67.css","static/chunks/pages/index-6f25f782349efe49.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
