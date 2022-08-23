3833fd342b6dfb93c5e3532192354547e353ff52
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2022-08-23 13:34:41 +0900

    スクロール時のヘッダスタイル調整

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,11 +4,31 @@ import HeaderNav from '@/components/layout/HeaderNav'
 import MobileNav from '@/components/layout/MobileNav'
 import HeaderForm from '@/components/layout/HeaderForm'
 import HeaderProfile from '@/components/layout/HeaderProfile'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter()
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
   return (
     <>
-      <header className={styles.header}>
+      <header
+        className={`${scrolled || router.pathname !== '/' ? 'scrolled' : ''} ${styles.header}`}
+      >
         <div className={styles.inner}>
           <HeaderLogo />
           <HeaderNav />
